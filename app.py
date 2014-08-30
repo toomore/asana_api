6245d8e42f4af0a3370193eac9b62e66c39a9a28
Toomore Chiang <toomore0929@gmail.com>
2014-08-30 02:36:13 +0000

    Add AsanaApi.oauth_authorize into app.

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 # -*- coding:utf8 -*-
 import setting
+from asanaapi import AsanaApi
 from flask import Flask
 
 app = Flask(__name__)
@@ -7,4 +8,7 @@ app.secret_key = setting.SESSION_KEY
 
 @app.route('/')
 def home():
-    return u'Hello world'
+    return u'Hello world <a href="%s">Login</a>' % AsanaApi.oauth_authorize(setting.OAUTHID, setting.OAUTHREDIRECT)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', debug=True)
