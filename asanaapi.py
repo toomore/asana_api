4b57917ac2991e0579a8c8f9f43a65d19b6ec8b2
Toomore Chiang <toomore0929@gmail.com>
2014-08-30 00:56:52 +0000

    Add sample.

--- asanaapi.py
+++ asanaapi.py
@@ -8,12 +8,13 @@ class AsanaApi(object):
     def __init__(self, api_key):
         self.api_key = api_key
         self.api_url = u'https://app.asana.com/api/1.0/'
+        self.session = Session()
 
     def _requests(self, method, path, params=None):
         path = urljoin(self.api_url, path)
-        session = Session()
-        raw_requests = Request(method, path, auth=(self.api_key, ''), params=params)
-        return session.send(raw_requests.prepare())
+        raw_requests = Request(method, path, auth=(self.api_key, ''),
+                params=params)
+        return self.session.send(raw_requests.prepare())
 
     def get(self, *args, **kwargs):
         return self._requests('GET', *args, **kwargs)
@@ -23,5 +24,10 @@ if __name__ == '__main__':
     asana = AsanaApi(setting.API_KEY)
     params = {'workspace': setting.WORKSPACE,
               'assignee': 'me'}
+    ## http://developer.asana.com/documentation/#tasks
     result = asana.get('./tasks', params=params)
-    print result.json()
+    for i in result.json()['data']:
+        for ele in i:
+            print ele, i[ele],
+        print
+    print len(result.json()['data'])
